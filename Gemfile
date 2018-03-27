39c4fa6ed96238a7c3826d4529b82e6d4f672d4f
Evan Wheeler <anon0mys87@gmail.com>
2018-03-27 14:38:42 -0600

    Navigation with nav bar and by student name on index page complete

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara'
   gem 'database_cleaner'
+  gem 'launchy'
   gem 'pry'
   gem 'rspec-rails'
   gem 'shoulda-matchers'
--- spec/features/students/user_can_see_a_list_of_students_spec.rb
+++ spec/features/students/user_can_see_a_list_of_students_spec.rb
@@ -14,4 +14,20 @@ describe 'User' do
       expect(page).to have_content(student_three.name)
     end
   end
+
+  context 'can use links to navigate' do
+    scenario 'student name links to show page' do
+      student_one = Student.create(name: 'Ian')
+      student_two = Student.create(name: 'Ali')
+      visit students_path
+      click_on "#{student_one.name}"
+
+      expect(current_path).to eq(student_path(student_one))
+
+      visit students_path
+      click_on "#{student_two.name}"
+
+      expect(current_path).to eq(student_path(student_two))
+    end
+  end
 end
